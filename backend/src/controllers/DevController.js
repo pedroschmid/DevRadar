9b41bd48d84894a774e95d771215018da66c1e4e
Pedro <pedro.schmid2311@gmail.com>
2020-01-17 20:13:53 -0300

    Commit

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray')
+const { findConnections, sendMessage } = require('../websocket')
 
 module.exports = {
     async index(request, response) {
@@ -43,6 +44,14 @@ module.exports = {
                 techs: techsArray,
                 location,
             })
+
+            // Filtrar as conexões que estao a 10km e que tenha as techs
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            )
+            
+            sendMessage(sendSocketMessageTo, 'new-dev', dev)
         }
 
         return response.json(dev)
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
 const routes = require('./routes')
+const { setupWebsocket } = require('./websocket')
 const port = 3333
 
 const app = express();
+const server = http.Server(app)
+
+setupWebsocket(server)
 
 mongoose.connect('mongodb+srv://root:root@cluster0-zp9uk.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,6 +20,6 @@ app.use(cors({}))
 app.use(express.json())
 app.use(routes)
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server running on port: ' + port);
 })
